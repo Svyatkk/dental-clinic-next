5fc0adbe53f9abe6de096f3033556428cd0850c3
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2026-02-24 15:26:59 +0200

    update

--- src/app/(services-about-Pages)/services/[name]/page.tsx
+++ src/app/(services-about-Pages)/services/[name]/page.tsx
@@ -1,31 +1,26 @@
-'use client'
-
-import { useParams } from "next/navigation"
 import { SERVICES } from "@/data/services.data"
-
+import { notFound } from "next/navigation"
 
 type Props = {
     params: Promise<{ name: string }>
 }
 
-export default async function Service({ params }: Props) {
+export default async function ServicePage({ params }: Props) {
     const { name } = await params
 
     const decodedName = decodeURIComponent(name)
 
     const found = SERVICES.find(s => s.name === decodedName)
 
+    if (!found) {
+        return notFound()
+    }
 
 
     return (
-        <>
-
-
-
-            <div>
-                {found?.name}
-            </div>
-
-        </>
+        <div style={{ padding: '20px' }}>
+            <h1>{found.name}</h1>
+            <p>Ціна: {found.price} грн</p>
+        </div>
     )
 }
