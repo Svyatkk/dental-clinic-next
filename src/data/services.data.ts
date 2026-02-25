5c88f3e22f561d6b299541505b9d5c0b12d22210
SviatoslaKondratiuk <ipz241_ksr@student.ztu.edu.ua>
2026-02-25 11:40:11 +0200

    update

--- src/data/services.data.ts
+++ src/data/services.data.ts
@@ -1,24 +1,38 @@
 import { ServicesType } from "@/types/services.types"
 export const SERVICES: ServicesType[] = [
     {
-
         name: 'Послуга',
         price: 200,
+        engName: 'Service',
+        hrefImg: '',
     },
+
     {
         name: 'Терапевтична стоматологія',
-        price: 200
+        price: 200,
+        engName: 'terapevtychna-stomatolohiia',
+        hrefImg: '',
+
     },
     {
         name: 'Ортодонтія',
-        price: 200
+        price: 200,
+        engName: 'ortodontiia',
+        hrefImg: ''
+
     },
     {
         name: 'Хірургія',
-        price: 200
+        price: 200,
+        engName: 'khirurhiia',
+        hrefImg: ''
+
     },
     {
         name: 'Лікування каналів зуба',
-        price: 200
+        price: 200,
+        engName: 'likuvannia-kanaliv-zuba',
+        hrefImg: ''
+
     }
 ]
